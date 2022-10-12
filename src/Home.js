837ebfe1a3804c8560acec9d6951951fd2624869
Lakshya Vyas <lakshyavyas987@gmail.com>
2022-10-12 11:43:52 +0530

    First commit Navbar Component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => {
+  return (
+    <Wrapper >Home</Wrapper>
+  )
+};
+
+const Wrapper = styled.section`
+
+`;
+
+export default Home
---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img className="logo" src="./images/logo.png" alt="my logo img" />
+      </NavLink>
+      <Dname>
+        Dharmendra Enterprises
+      </Dname>
+      <Nav />
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .logo {
+    height: 7rem;
+  }
+`;
+
+const Dname = styled.h2`
+font-size: 2.5rem;
+font-weight: bolder;
+text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.283);
+
+
+@media (max-width:1000px) {
+    font-size:2rem;
+}
+@media (max-width:920px) {
+    display:none;
+}
+@media (max-width:768px) {
+    font-size:2rem;
+    display:block;
+}
+@media (max-width:390px){
+    font-size:1rem;
+}
+`;
+
+export default Header;
